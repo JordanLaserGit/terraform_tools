70d76fe442689a068e99b2c6b493b11c5183e2e2
Jordan Laser <jordan.science.laser@gmail.com>
2023-03-15 14:34:15 -0500

    convert vpc, write, and validate

--- python/boto3_2_terraform.py
+++ python/boto3_2_terraform.py
@@ -11,39 +11,74 @@ class Converter():
         
     def convert(self):
 
+        self.convert_vpc()
+        self.convert_instance()
+        # self.convert_subnet()
 
-       
+
+    def convert_vpc(self):
+
         for jvpc in self.Response.ec2_responses['vpcs']['Vpcs']:
-             name = 'main'
-             cidr_block = jvpc['CidrBlock']
-             instance_tenancy = jvpc['InstanceTenancy']
+             # Find vpcs name
+            tags = self.Response.ec2_responses['tags']['image']
+            for jtag in tags:
+                if jtag['Key'] == 'Name':
+                    name = jtag['Value']
+                    break
+                     
+            cidr_block       = jvpc['CidrBlock']
+            instance_tenancy = jvpc['InstanceTenancy']
 
-             vpc_string = f'\"resource\" \"aws_vpc\" \"{name}\" {{ \n'
+            vpc_string = f'resource \"aws_vpc\" \"{name}\" {{ \n'
 
-             vpc_string += f'cidr_block = \"{cidr_block}\"\n'
-             vpc_string += f'cidr_block = \"{instance_tenancy}\"\n'
+            vpc_string += f'  cidr_block       = \"{cidr_block}\"\n'
+            vpc_string += f'  instance_tenancy = \"{instance_tenancy}\"\n\n'
 
-             vpc_string += f'tags = {{\"{instance_tenancy}\"\n'
-            #  for pair in self.Response.ec2_responses['tags']
+            vpc_string += f'  tags = {{\n'
+            for ktag in tags:
+                key = ktag['Key']
+                val = ktag['Value']
+                vpc_string += f'    {key} = \"{val}\"\n'
 
+            vpc_string += '  '
+            vpc_string += f'}}\n '
+            vpc_string += f'}}\n\n'
 
+            self.blocks.append(vpc_string)
+                 
+    def convert_instance(self):
+        pass
+
+    def convert_subnet(self):
         pass
 
     def write(self):
-        pass
+
+        with open(self.tf_file,'w') as f:
+            for jblock in self.blocks:
+                f.write(jblock)
 
     def validate(self):
 
         # cd into where terraform is output
-        # 
+        dir = os.path.dirname(os.path.realpath(self.tf_file))
+        os.chdir(dir) 
+
         # terraform init 
-        # 
+        print(f'Initializing...')
+        subprocess.run(["terraform","init"])
+
         # terraform fmt
-        # 
+        print(f'Validating Terraform format...')
+        subprocess.run(["terraform","fmt"])
+
         # terraform validate
-        # 
+        print(f'Validating Terraform config...')
+        subprocess.run(["terraform","validate"])
+
         # terraform plan 
-        pass       
+        print(f'Validating Terraform plan...')
+        subprocess.run(["terraform","plan"])   
 
 def main():
 
@@ -54,6 +89,7 @@ def main():
         response.gen_vpc_response()
 
         converter = Converter(response)
+        converter.tf_file = '/home/jlaser/code/terraform_tools/data/dev_file.tf'
         converter.convert()
         converter.write()
         converter.validate()
