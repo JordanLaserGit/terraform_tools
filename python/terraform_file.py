a1b6e0c0a1d0b23f02dfc5e560ab69b81b35a038
Jordan Laser <jordan.science.laser@gmail.com>
2023-03-17 15:17:18 -0500

    Fixed write function

--- python/terraform_file.py
+++ python/terraform_file.py
@@ -6,36 +6,38 @@ class TerraformFile():
         self.file  = ''
         self.Blocks   = [] 
 
-    def create_block_string(self):
+    def create_block_string(self,jBlk,depth=0):
         """
         Add dictionary to block string line by line
         """
+        if type(jBlk) is dict:
+            file_string = ''
+            dict_loop = jBlk
+        else:
+            file_string = jBlk.block_str
+            dict_loop = jBlk.args
+        for key in dict_loop:  
+            val = dict_loop[key]        
 
-        file_string = ''
-        for jBlk in self.Blocks:
-            depth = 0
-            file_string += jBlk.block_str
-            for key in jBlk.args:  
-                val = jBlk.args[key]        
-
-                if type(val) is not dict:
-                    if val is True:  val = "true"
-                    if val is False: val = "false"                    
-                    file_string += f'{key}       = \"{val}\"\n'.rjust(2,' ')
-                else:
-                    depth += 1
-                    file_string += f'{key}       = {{\n'.rjust(2,' ')
-                    file_string = self.create_block_string(val,2+2*depth)
-                    file_string += f'}}\n'
-                    depth -= 1 
-
-            file_string += f'}}\n'
+            if type(val) is not dict:
+                if val is True:  val = "true"
+                if val is False: val = "false"                    
+                file_string += f'{key}       = \"{val}\"\n'.rjust(2,' ')
+            else:
+                depth += 1
+                file_string += f'{key}       = {{\n'.rjust(2,' ')
+                file_string += self.create_block_string(val,2+2*depth)
+                file_string += f'}}\n'
+                depth -= 1         
 
         return file_string
 
     def write(self):
 
-        file_string = self.create_block_string()
+        file_string = ''
+        for jBlk in self.Blocks:
+            file_string += self.create_block_string(jBlk)
+            file_string += f'}}\n'
 
         with open(self.file,'w') as f:
             f.write(file_string)  
